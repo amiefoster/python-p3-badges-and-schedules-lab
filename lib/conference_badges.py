8733c8235abe54c345e9f66a02e8df665c3244fa
Amie Foster <amie.n.foster@gmail.com>
2023-01-29 21:42:01 -0600

    2/4 completed

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,17 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    return [f"Hello, my name is {name}." for name in names]
 
 def assign_rooms(names):
-    return None
+    roomNum = len(names)
+    assignments = []
+    while roomNum > len(names):
+        assignments.append(f'Hello, {names[roomNum - 1]}! You\'ll be assigned to room {roomNum}!')
+
+    print(assignments)
+    return assignments
 
 def printer(names):
     return None
